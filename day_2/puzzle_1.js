a2ef248c799f915a4eeb57328db3e918abf45d85
Darryl Pargeter <darryl.pargeter@schemeserve.com>
2021-12-02 12:04:00 +0000

    updated day 2 to parse using a generator funciton

--- day_2/puzzle_1.js
+++ day_2/puzzle_1.js
@@ -10,6 +10,14 @@ const testData = [
   'forward 2',
 ];
 
+function* parse(data) {
+  for (const line of data) {
+    const [op, value] = line.split(' ');
+
+    yield { op, value: +value };
+  };
+}
+
 function formatData(data) {
   return data.map(line => {
     const [op, value] = line.split(' ');
@@ -21,19 +29,19 @@ function puzzle1fn(data) {
   let forword = 0;
   let depth = 0;
 
-  data.forEach(item => {
-    if (item.op === 'forward') {
-      forword += item.value;
+  for (const { op, value } of parse(data)) {
+    if (op === 'forward') {
+      forword += value;
     }
 
-    if (item.op === 'up') {
-      depth -= item.value;
+    if (op === 'up') {
+      depth -= value;
     }
 
-    if (item.op === 'down') {
-      depth += item.value;
+    if (op === 'down') {
+      depth += value;
     }
-  });
+  } 
 
   return forword * depth;
 }
@@ -43,20 +51,20 @@ function puzzle2fn(data) {
   let depth = 0;
   let aim = 0;
 
-  data.forEach(item => {
-    if (item.op === 'forward') {
-      forword += item.value;
-      depth += aim * item.value;
+  for (const { op, value } of parse(data)) {
+    if (op === 'forward') {
+      forword += value;
+      depth += aim * value;
     }
 
-    if (item.op === 'up') {
-      aim -= item.value;
+    if (op === 'up') {
+      aim -= value;
     }
 
-    if (item.op === 'down') {
-      aim += item.value;
+    if (op === 'down') {
+      aim += value;
     }
-  });
+  };
 
   return forword * depth;
 }
@@ -64,8 +72,8 @@ function puzzle2fn(data) {
 async function main() {
   const data = await readFileAsArray(path.resolve(__dirname, '../day_2/data.txt'));
   const formattedData = formatData(data);
-  const puzzle1 = puzzle1fn(formattedData);
-  const puzzle2 = puzzle2fn(formattedData);
+  const puzzle1 = puzzle1fn(data);
+  const puzzle2 = puzzle2fn(data);
 
   console.log(`puzzle1 output: ${puzzle1}`);
   console.log(`puzzle2 output: ${puzzle2}`);
