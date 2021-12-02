354f888f64842d5cd6e03f67e53f74afa04ceebd
Darryl Pargeter <darryl.pargeter@schemeserve.com>
2021-12-02 10:30:14 +0000

    compeleted day 2 puzzle 2

--- day_2/puzzle_1.js
+++ day_2/puzzle_1.js
@@ -35,7 +35,29 @@ function puzzle1fn(data) {
     }
   });
 
-  console.log({ forword, depth });
+  return forword * depth;
+}
+
+function puzzle2fn(data) {
+  let forword = 0;
+  let depth = 0;
+  let aim = 0;
+
+  data.forEach(item => {
+    if (item.op === 'forward') {
+      forword += item.value;
+      depth += aim * item.value;
+    }
+
+    if (item.op === 'up') {
+      aim -= item.value;
+    }
+
+    if (item.op === 'down') {
+      aim += item.value;
+    }
+  });
+
   return forword * depth;
 }
 
@@ -43,8 +65,10 @@ async function main() {
   const data = await readFileAsArray(path.resolve(__dirname, '../day_2/data.txt'));
   const formattedData = formatData(data);
   const puzzle1 = puzzle1fn(formattedData);
+  const puzzle2 = puzzle2fn(formattedData);
 
   console.log(`puzzle1 output: ${puzzle1}`);
+  console.log(`puzzle2 output: ${puzzle2}`);
 }
 
 main();
